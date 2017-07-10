36c356c30d6ed79ca11eb82cc52253b81d9c9b44
musique-live <taw84@cornell.edu>
2017-07-10 19:53:50 -0400

    Initial Commit

---
+++ ExampleViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleViewController
+//
+//  Created by Tara Wilson on 7/10/17.
+//  Copyright © 2017 twil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
